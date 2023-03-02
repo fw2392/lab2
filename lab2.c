eb58d64b8922836b23c6361aac36ff76cfcf4d36
fw2392 <125504980+fw2392@users.noreply.github.com>
2023-03-01 19:47:26 -0500

    Add files via upload

--- lab2.c
+++ lab2.c
@@ -163,8 +163,17 @@ int main()
       else if(packet.keycode[0] == 0x2A){//back space
         if(rownum >= 21){
           fbputchar(' ',rownum,colnum);
+          int newcol = colnum;
+          int new = part_Idex-1;
+          while(new > 0){
+            fbputchar(part_message[new-1],rownum,newcol);
+            new = new -1;
+            newcol = newcol + 1;
+          } 
           colnum = colnum - 1;
           charIdex = charIdex - 1;
+          
+          
           if((colnum < 0 && rownum == 22)){
             colnum = 63;
             rownum = rownum - 1;
